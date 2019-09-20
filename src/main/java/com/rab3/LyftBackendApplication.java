81bd8583e60c0d60566f42dbdb451ea8a80533a2
hsakarp <kunwar.prakash.man@gmail.com>
2019-09-19 21:22:21 -0400

    Project created with driver sign  up controller

---
+++ src/main/java/com/rab3/LyftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyftBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rab3/LyftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
